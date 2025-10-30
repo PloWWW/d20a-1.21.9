e96e8e8e3deeb7cb85b8b422cd7c78aa387045a9
Plow <153658273+PloWWW@users.noreply.github.com>
2025-10-30 18:20:50 +0300

    Init

---
+++ src/main/java/org/plw/D20AClient.java
@@ -0,0 +1,10 @@
+﻿package org.plw;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class D20AClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/org/plw/D20ADataGenerator.java
@@ -0,0 +1,11 @@
+package org.plw;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class D20ADataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
